d62d413831b9bf84b1fe61c90d3dffd5fd210b5c
Wole Ajewole <wolemercy@gmail.com>
2021-11-08 05:55:38 +0100

    Add monty-hall python script

---
+++ monty-hall.py
@@ -0,0 +1,68 @@
+# The Monty Hall Problem
+
+import random
+
+# Instantiating a random treasure map
+def single_run():
+    locations = ['wasteland', 'wasteland', 'wasteland']
+    treasure_index = random.randint(0, 2)
+    locations[treasure_index] = 'one piece'
+    return locations
+
+# Luffy's first choice
+def luffy():
+    luffy_first_choice = random.randint(0, 2)
+    return luffy_first_choice
+
+# Monty's choice of a location that is neither Luffy's choice nor the treasure location
+def monty(locations, luffy_first_choice):
+    monty_choice = 0
+
+    while monty_choice == luffy_first_choice or locations[monty_choice] == 'one piece':
+        monty_choice += 1
+    
+    return monty_choice
+
+# switch Luffy's choice
+def luffy_switch(luffy_first_choice, monty_choice):
+    
+    luffy_switch_choice = 0
+
+    while luffy_switch_choice == luffy_first_choice or luffy_switch_choice == monty_choice:
+        luffy_switch_choice += 1
+
+    return luffy_switch_choice
+
+# output to be displayed
+def output(stick, switch, trials):
+    stick_percent = round((stick/trials) * 100)
+    switch_percent = round((switch/trials) * 100)
+    
+    print(f'Luffy found One Piece {stick_percent} % of the time when he decided to stick to his initial choice ')
+    print(f'Luffy found One Piece {switch_percent} % of the time when he decided to switch his initial choice')
+
+
+print('The Monty Hall Problem')
+trials = int(input('Enter the number of trials:  '))
+
+# Luffy sticks
+stick_count = 0
+
+# Luffy switches
+switch_count = 0
+
+for i in range(trials):
+    locations = single_run()
+    luffy_first_choice = luffy()
+    monty_choice = monty(locations, luffy_first_choice)
+    luffy_switch_choice = luffy_switch(luffy_first_choice, monty_choice)
+
+    if locations[luffy_first_choice] == 'one piece':
+        stick_count += 1
+    
+    elif locations[luffy_switch_choice] == 'one piece':
+        switch_count += 1
+
+output(stick_count, switch_count, trials) 
+
+ 
